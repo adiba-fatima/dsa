dc81a430aa35d1e79534c0291343d6b0a58004d8
Adiba Fatima <adibafatima1006@gmail.com>
2025-12-07 19:12:55 +0600

    Add files via upload

---
+++ Lab/DSA in C/insertafterpos.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int LA[]={10,20,30,40,50};
+    int k=5;
+    int pos,value;
+    scanf("%d%d",&pos,&value);
+    int i=k;
+    while(i>pos)
+    {
+        LA[i]=LA[i-1];
+        i--;
+    }
+    LA[pos]=value;
+    k++;
+    i=1;
+    while(i<=k)
+    {
+        printf("%d ",LA[i-1]);
+        i++;
+    }
+}
