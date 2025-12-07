8a3e0533729764faa7a8f21978242efece0d6a84
Adiba Fatima <adibafatima1006@gmail.com>
2025-12-07 19:10:24 +0600

    Add files via upload

---
+++ Lab/DSA in C/traverse.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int LA[]={10,20,30,40,50};
+    int k=5;//number of element
+    int i=1;
+    while(i<=k)
+    {
+        printf("%d ",LA[i-1]);
+        i++;
+    }
+return 0;
+}
